053ec4c0f79aac8d88839617bfe11ec9fa40675f
Dakota Newman <dakota.newman@yahoo.com>
2022-02-06 13:32:02 -0800

    added squared sums and create phone number functions

--- code_wars.py
+++ code_wars.py
@@ -1,4 +1,7 @@
 def likes(names):
+    """
+    takes a list of names and depending on length, returns who like it
+    """
     res = ''
     if len(names) == 0:
         res = 'no one likes this'
@@ -17,3 +20,26 @@ likes(['Peter'])
 likes(['Peter', 'Alex'])
 likes(['Peter', 'Alex', 'Mark'])
 likes(['Peter', 'Alex', 'Mark', 'Max'])
+
+###############################################################
+
+def square_sum(numbers):
+    """
+    takes list of numbers and returns a sum of the numbers squared
+    """
+    squared_Sum = sum([x**2 for x in numbers])
+    return squared_Sum
+
+square_sum([1, 2])
+
+###############################################################
+
+def create_phone_number(n):
+    strings = [str(x) for x in n]
+    area_code = ''.join(strings[:3])
+    exchange_code = ''.join(strings[3:6])
+    line_number = ''.join(strings[6:])
+    res = '(' + area_code + ')' + ' ' + exchange_code + '-' + line_number
+    return res
+
+create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
